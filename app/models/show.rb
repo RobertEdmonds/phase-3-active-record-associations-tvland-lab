cd846b4564f998bf11357ba7c4df22b57754f595
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-06-21 13:35:06 -0600

    commit tree a41130c8488e7c53e9aecd6c810ff998e98337e1

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network 
+    
+    def actors_list
+        # first_name = self.actors.map {|t| t.first_name}
+        # last_name = self.actors.map {|t| t.last_name}
+        # first_name_string = first_name.join("")
+        # last_name_string = last_name.join("")
+        # "#{first_name_string} #{last_name_string}"
+        self.actors.map {|actor| "#{actor.first_name} #{actor.last_name}"}
+    end
 end
